0d43134db79f6e28a0db5a5ab7e9f19d0611694c
p512 <point6000x+gh@gmail.com>
2015-01-08 23:11:20 +0100

    Fixed crash
    Ghostly / colorFadout implementation faulty. Was using letter instead of
    current.

--- src/colorFadeout.cpp
+++ src/colorFadeout.cpp
@@ -7,7 +7,7 @@ colorFadeout::colorFadeout()
 
 void colorFadeout::update(char letter, unsigned int current, unsigned int length)
 {
-    m_RGB.r = (double)letter/length * 0.9;
+    m_RGB.r = (double)current/length * 0.9;
     m_RGB.g = m_RGB.r;
     m_RGB.b = m_RGB.r;
 }
